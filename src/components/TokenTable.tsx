aac8c0f8c9dc78c12855e34f3aac0585f50d12b4
maestroDever <maestro.official@icloud.com>
2025-02-10 09:19:23 -0500

    feat: implement pagination

--- src/components/TokenTable.tsx
+++ src/components/TokenTable.tsx
@@ -5,9 +5,13 @@ type TokenTableProps = {
   tokenList: Token[];
   loading: boolean;
   error: string | null;
-}
+};
 
-export default function TokenTable({ tokenList, loading, error }: TokenTableProps) {
+export default function TokenTable({
+  tokenList,
+  loading,
+  error
+}: TokenTableProps) {
   return (
     <table className="min-w-full divide-y divide-gray-300 dark:divide-gray-700">
       <thead>
@@ -77,94 +81,102 @@ export default function TokenTable({ tokenList, loading, error }: TokenTableProp
       <tbody className="divide-y divide-gray-200 bg-white dark:divide-gray-700 dark:bg-gray-800">
         {loading && (
           <tr>
-            <td colSpan={100} className="text-center p-10">Loading...</td>
+            <td colSpan={100} className="text-center p-10">
+              Loading...
+            </td>
           </tr>
         )}
         {error && (
           <tr>
-            <td colSpan={100} className="text-center p-10 text-red-500">Error: {error}</td>
+            <td colSpan={100} className="text-center p-10 text-red-500">
+              Error: {error}
+            </td>
           </tr>
         )}
-        {tokenList.map((token) => (
-          <tr key={token.tick}>
-            <td className="sticky left-0 whitespace-nowrap py-5 pl-4 px-3 text-sm bg-white dark:bg-gray-800">
-              <div className="flex items-center">
-                <div className="size-11 shrink-0">
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img
-                    alt=""
-                    src={`data:image/svg+xml,${encodeURIComponent(
-                      '<svg width="256" height="256" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"><circle cx="128" cy="128" r="128" fill="#808080"/></svg>'
-                    )}`}
-                    className="size-11 rounded-full"
-                  />
-                </div>
-                <div className="ml-4">
-                  <div className="font-medium text-gray-900 dark:text-white">
-                    {token.tick}
+        {!loading &&
+          !error &&
+          tokenList.map((token) => (
+            <tr key={token.tick}>
+              <td className="sticky left-0 whitespace-nowrap py-5 pl-4 px-3 text-sm bg-white dark:bg-gray-800">
+                <div className="flex items-center">
+                  <div className="size-11 shrink-0">
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img
+                      alt=""
+                      src={`data:image/svg+xml,${encodeURIComponent(
+                        '<svg width="256" height="256" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"><circle cx="128" cy="128" r="128" fill="#808080"/></svg>'
+                      )}`}
+                      className="size-11 rounded-full"
+                    />
+                  </div>
+                  <div className="ml-4">
+                    <div className="font-medium text-gray-900 dark:text-white">
+                      {token.tick}
+                    </div>
                   </div>
                 </div>
-              </div>
-            </td>
-            <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-              <div className="text-gray-900 dark:text-white">{token.max}</div>
-              {/* <div className="mt-1 text-gray-500 dark:text-gray-400">
+              </td>
+              <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                <div className="text-gray-900 dark:text-white">{token.max}</div>
+                {/* <div className="mt-1 text-gray-500 dark:text-gray-400">
                 {token.lim}
               </div> */}
-            </td>
-            <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
-              <div className="text-gray-900 dark:text-white">
-                {formatPercentage((token.pre / token.max) * 100)}
-              </div>
-              <div className="mt-1 text-gray-500 dark:text-gray-400">
-                {token.pre}
-              </div>
-              {/* <div className="mt-1 text-gray-500 dark:text-gray-400 truncate max-w-[200px]">
+              </td>
+              <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
+                <div className="text-gray-900 dark:text-white">
+                  {formatPercentage((token.pre / token.max) * 100)}
+                </div>
+                <div className="mt-1 text-gray-500 dark:text-gray-400">
+                  {token.pre}
+                </div>
+                {/* <div className="mt-1 text-gray-500 dark:text-gray-400 truncate max-w-[200px]">
                 {token.to}
               </div> */}
-            </td>
-            <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
-              <div className="text-gray-900 dark:text-white">
-                {formatPercentage((token.minted / token.max) * 100)}
-              </div>
-              <div className="mt-1 text-gray-500 dark:text-gray-400">
-                {token.minted}
-              </div>
-            </td>
-            <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
-              <div className="text-gray-900 dark:text-white">{token.dec}</div>
-            </td>
-            <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
-              <div className="text-gray-900 dark:text-white">
-                {token.opScoreAdd}
-              </div>
-              <div className="mt-1 text-gray-500 dark:text-gray-400">
-                {token.opScoreMod}
-              </div>
-            </td>
-            <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
-              <div className="text-gray-900 dark:text-white">{token.state}</div>
-            </td>
-            <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
-              <div className="text-gray-900 dark:text-white truncate max-w-[200px]">
-                {token.hashRev}
-              </div>
-            </td>
-            <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
-              <div className="text-gray-900 dark:text-white">
-                {token.mtsAdd}
-              </div>
-            </td>
-            <td className="relative whitespace-nowrap py-5 pl-3 px-4 text-right text-sm font-medium">
-              <a
-                href="#"
-                className="text-indigo-600 hover:text-indigo-900 dark:text-indigo-400 dark:hover:text-indigo-300"
-              >
-                View Details<span className="sr-only">, {token.tick}</span>
-              </a>
-            </td>
-          </tr>
-        ))}
+              </td>
+              <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
+                <div className="text-gray-900 dark:text-white">
+                  {formatPercentage((token.minted / token.max) * 100)}
+                </div>
+                <div className="mt-1 text-gray-500 dark:text-gray-400">
+                  {token.minted}
+                </div>
+              </td>
+              <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
+                <div className="text-gray-900 dark:text-white">{token.dec}</div>
+              </td>
+              <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
+                <div className="text-gray-900 dark:text-white">
+                  {token.opScoreAdd}
+                </div>
+                <div className="mt-1 text-gray-500 dark:text-gray-400">
+                  {token.opScoreMod}
+                </div>
+              </td>
+              <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
+                <div className="text-gray-900 dark:text-white">
+                  {token.state}
+                </div>
+              </td>
+              <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
+                <div className="text-gray-900 dark:text-white truncate max-w-[200px]">
+                  {token.hashRev}
+                </div>
+              </td>
+              <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 dark:text-gray-400">
+                <div className="text-gray-900 dark:text-white">
+                  {token.mtsAdd}
+                </div>
+              </td>
+              <td className="relative whitespace-nowrap py-5 pl-3 px-4 text-right text-sm font-medium">
+                <a
+                  href="#"
+                  className="text-indigo-600 hover:text-indigo-900 dark:text-indigo-400 dark:hover:text-indigo-300"
+                >
+                  View Details<span className="sr-only">, {token.tick}</span>
+                </a>
+              </td>
+            </tr>
+          ))}
       </tbody>
     </table>
   );
